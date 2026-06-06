d2bb65ebf0466ded0c3f54804967b0b8c6cdf697
sarath <03sarath@gmail.com>
2026-06-06 22:50:59 +0530

    Initial commit

--- awsinfra_deploy/awsinfra_deploy_stack.py
+++ awsinfra_deploy/awsinfra_deploy_stack.py
@@ -57,9 +57,9 @@ class AwsinfraDeployStack(Stack):
         source_action = codepipeline_actions.GitHubSourceAction(
             action_name='GitHub_Source',
             owner='03sarath',  # Replace with your GitHub username
-            repo='aws-ckd-gettingstarted',  # Replace with your repository name
-            branch='main',
-            oauth_token=SecretValue.secrets_manager('github-token'),
+            repo='awsinfra-deploy',  # Replace with your repository name
+            branch='master',
+            oauth_token=SecretValue.secrets_manager('github-token-june'),
             output=source_output
         )
 
